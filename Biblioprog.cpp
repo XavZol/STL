eadc21db419b2219b64bae7cdf752821b0deaf5f
XavZol <alansolis707@gmail.com>
2025-11-09 15:16:14 -0600

    Deque - doble cola

--- Biblioprog.cpp
+++ Biblioprog.cpp
@@ -1,31 +1,35 @@
-//Contenedor Lineal - vectores
+//Contenedor Lineal - deque (doble cola)
 
 #include<iostream>
-#include<vector>
+#include<deque>
 using namespace std;
 int main(){
-	vector<int> numeros; //Arreglos enteros
-	
-    numeros.push_back(5); //agrega elementos al final del arreglo
-	numeros.push_back(10);
-	numeros.push_back(1);
-	numeros.push_back(14);
-	numeros.push_back(8);
-	
-	numeros[2] = 7; //agregar elementos en la posicion
-	
-	//Mostrar los elementos del vector
-	for(int i=0; i<numeros.size(); i++)
-		cout<<numeros[i]<<"|";
-		
-//	numeros.pop_back(); //elimina el ultimo elemento del arreglo
- 
-//	numeros.erase(numeros.begin()+1,numeros.begin()+3);
+    deque<char> letras;
 
-	cout<<endl;
-	//Mostrar los elementos del vector
-	for(int i=0; i<numeros.size(); i++)
-		cout<<numeros[i]<<"|";
-	
+    // A B C D E F
+
+    letras.push_front('C'); //Agregamos un elemento por el principio 
+	letras.push_front('B');
+    letras.push_front('A');
+
+    letras.push_back('D'); //Agregamos un elemento al final
+    letras.push_back('E');
+    letras.push_back('F');
+
+    // Mostramos los elemento sdel deque
+    for(int i=0; i<letras.size();i++){
+        cout<<letras[i]<<"|";
+    }
+
+    letras.pop_front(); //elimina elmentos del frente
+    letras.pop_back(); //Elimina un elemnto del final
+
+    cout<<endl;
+    
+     // Mostramos los elemento sdel deque
+    for(int i=0; i<letras.size();i++){
+        cout<<letras[i]<<"|";
+    }
+
 	return 0;
 }
